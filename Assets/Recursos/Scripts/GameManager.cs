73fa60be2688f889ba069134583f2d71587511b8
matrakei <163850982+matrakei@users.noreply.github.com>
2024-08-20 11:58:29 -0300

    CAMIBO

---
+++ Assets/Recursos/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+    private float cantidadPuntos;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+
+    public void Sumarpuntos(float puntos)
+    {
+        cantidadPuntos += puntos;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+            {
+
+            SceneManager.LoadScene("PlataformasZorrito 4to D 1");
+        }
+    }
+
+
+
+
+}
